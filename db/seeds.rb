437d8d10b6852ca59e0e83e8406cdbc30a4a54a6
YunDuali <duyun530@tamu.edu>
2023-02-22 18:09:29 -0600

    Add my movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,10 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
    :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-   :release_date => '13-Aug-2021'}
+   :release_date => '13-Aug-2021'},
+  {:title => 'WildLife', :rating => 'G',
+   :release_date => '20-Jan-2018'}
+
 ]
 
 more_movies.each do |movie|
